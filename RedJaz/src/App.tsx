2df63b24dc7ce928e49aa8daf6f53eeb82f23e44
Jacobo <tadeo.jacobo6@gmail.com>
2024-07-18 20:51:55 -0400

    defining first components

--- RedJaz/src/App.tsx
+++ RedJaz/src/App.tsx
@@ -1,6 +1,17 @@
+import './globals.css'
+import { Route, Routes } from 'react-router-dom'
+
 const App = () => {
   return (
-    <div>App</div>
+    <main className='Flex h-screen'>
+      <Routes>
+        {/* Public routes */}
+        <Route path='/sign-in' element={<SigninForm />} />
+
+        {/* Private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   )
 }
 
