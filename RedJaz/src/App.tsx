3f4940b081ab3c3cd35877f661d116620f75a249
Jacobo <tadeo.jacobo6@gmail.com>
2024-07-19 23:04:45 -0400

    more routes

--- RedJaz/src/App.tsx
+++ RedJaz/src/App.tsx
@@ -1,3 +1,7 @@
+import AuthLayout from './_auth/AuthLayout'
+import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+import { Home } from './_root/pages'
 import './globals.css'
 import { Route, Routes } from 'react-router-dom'
 
@@ -6,7 +10,11 @@ const App = () => {
     <main className='Flex h-screen'>
       <Routes>
         {/* Public routes */}
+        <Route>
+          <Route element={<AuthLayout />}>
         <Route path='/sign-in' element={<SigninForm />} />
+        <Route path='/sign-up' element={<SignupForm />} />
+        </Route>
 
         {/* Private routes */}
         <Route index element={<Home />} />
