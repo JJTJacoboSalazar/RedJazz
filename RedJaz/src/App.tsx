48a4c0192c7d31a5a40b6d6958a0d6021f720ac5
Jacobo <tadeo.jacobo6@gmail.com>
2024-07-20 23:52:43 -0400

    adding Shadcn

--- RedJaz/src/App.tsx
+++ RedJaz/src/App.tsx
@@ -1,6 +1,7 @@
 import AuthLayout from './_auth/AuthLayout'
 import SigninForm from './_auth/forms/SigninForm'
 import SignupForm from './_auth/forms/SignupForm'
+import RootLayout from './_root/RootLayout'
 import { Home } from './_root/pages'
 import './globals.css'
 import { Route, Routes } from 'react-router-dom'
@@ -10,14 +11,15 @@ const App = () => {
     <main className='Flex h-screen'>
       <Routes>
         {/* Public routes */}
-        <Route>
           <Route element={<AuthLayout />}>
         <Route path='/sign-in' element={<SigninForm />} />
         <Route path='/sign-up' element={<SignupForm />} />
         </Route>
 
         {/* Private routes */}
+        <Route element={<RootLayout/>} >
         <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   )
