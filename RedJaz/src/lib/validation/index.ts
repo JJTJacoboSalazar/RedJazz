90da2f1b58c835f1c422fe82cfe12fbc37cf68a1
Jacobo <tadeo.jacobo6@gmail.com>
2024-07-23 22:51:30 -0400

    New inputs

---
+++ RedJaz/src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import { z } from "zod"
+
+export const SingupValidation = z.object({
+    name: z.string().min(2,{ message: "Name must be at least 2 characters long" }),
+    username: z.string().min(2,{ message: "Username must be at least 2 characters long" }),
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z.string().min(6,{ message: "Password must be at least 6 characters long" }),
+  })
