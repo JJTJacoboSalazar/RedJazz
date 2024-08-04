61849e46ad8a87ec2fa400a65471614c5da07b19
Jacobo <tadeo.jacobo6@gmail.com>
2024-08-03 23:57:00 -0400

    adding leftSidebar

--- RedJaz/src/components/shared/LeftSidebar.tsx
+++ RedJaz/src/components/shared/LeftSidebar.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Button } from '../ui/button'
+import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
+import { useEffect } from 'react'
+import { useUserContext } from '@/context/AuthContext'
 
 const LeftSidebar = () => {
+  const {mutate: signOut, isSuccess} = useSignOutAccount()
+  const navigate = useNavigate()
+  const {user} = useUserContext()
+
+  useEffect(() => {
+    if(isSuccess) {
+      navigate(0)
+    }
+  }, [isSuccess])
   return (
-    <div>LeftSidebar</div>
+    <nav className='leftsidebar' >
+      <div className='flex flex-col gap-11' >
+      <Link to='/' className='flex gap-3 items-center'>
+        <img src="/assets/images/logo.svg" alt="logo" width={170} height={36} />
+        </Link>
+
+          <Link to={`/profile/${user.id}`} >
+          </Link>
+      </div>
+    </nav>
   )
 }
 
