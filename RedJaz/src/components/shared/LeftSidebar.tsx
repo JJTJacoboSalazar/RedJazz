14ab5bec22c9817d33c4d6ce3744d5c43cbb0194
Jacobo <tadeo.jacobo6@gmail.com>
2024-08-05 20:05:22 -0400

    adding constants links

--- RedJaz/src/components/shared/LeftSidebar.tsx
+++ RedJaz/src/components/shared/LeftSidebar.tsx
@@ -18,11 +18,17 @@ const LeftSidebar = () => {
     <nav className='leftsidebar' >
       <div className='flex flex-col gap-11' >
       <Link to='/' className='flex gap-3 items-center'>
-        <img src="/assets/images/logo.svg" alt="logo" width={170} height={34} />
+        <img src="/assets/images/logo.svg" alt="logo" width={170} height={36} />
         </Link>
 
-          <Link to={`/profile/${user.id}`} >
+          <Link to={`/profile/${user.id}`} className='flex gap-3 items-center'  >
+          <img src={user.imageUrl || "/assets/icons/profile-placeholder.svg" } alt="profile" className='h-14 w-14 rounded-full' />
+          <div className='flex flex-col' >
+            <p className='body-bold'>{user.name}</p>
+            <p className='small-regular text-light-3'>{user.username}</p>
+          </div>
           </Link>
+      
       </div>
     </nav>
   )
