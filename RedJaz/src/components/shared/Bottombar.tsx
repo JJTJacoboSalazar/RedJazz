fd31b01b7385b9e76538488fea3d927e54b48ed5
Jacobo <tadeo.jacobo6@gmail.com>
2024-08-06 16:02:07 -0400

    Bottombar for mobile devices!

--- RedJaz/src/components/shared/Bottombar.tsx
+++ RedJaz/src/components/shared/Bottombar.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { bottombarLinks } from '@/constants'
+import {Link, useLocation} from 'react-router-dom'
 
 const Bottombar = () => {
+  const {pathname} = useLocation()
   return (
-    <div>Bottombar</div>
+    <section className="bottom-bar" >
+      {bottombarLinks.map((link) =>{
+              const isActive = pathname === link.route
+              return (
+                  <Link to={link.route} key={link.label} className={`${isActive && 'bg-primary-500 rounded-[10px]'} gap-1 flex flex-col items-center justify-center text-sm text-gray-500 group hover:text-white transition-all duration-300 ease-in-out p-2 ${isActive && 'text-white'
+                  }`} >
+                    <img 
+                    src={link.imgURL} 
+                    alt={link.label} 
+                    width={20}
+                    height={20}
+                    className={` ${isActive && 'invert-white'}`} />
+                    <p className='tiny-medium text-light-2' >{link.label}</p>
+                  </Link>
+
+
+              )
+            })}
+    </section>
   )
 }
 
