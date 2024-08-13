ca79ef6c731519b6efb1f98b7b8ff5336e246ecd
Jacobo <tadeo.jacobo6@gmail.com>
2024-08-13 15:13:19 -0400

    queryKeys added

--- RedJaz/src/lib/react-query/queriesAndMutations.ts
+++ RedJaz/src/lib/react-query/queriesAndMutations.ts
@@ -1,7 +1,7 @@
 import {useQuery, useMutation, useQueryClient, useInfiniteQuery} from '@tanstack/react-query';
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
+import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
 import { INewPost, INewUser } from '@/types';
-import { CreatePost } from '@/_root/pages';
+import { QUERY_KEYS } from './queryKeys';
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -19,5 +19,18 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
     return useMutation({
         mutationFn: signOutAccount
+    });
+}
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
+        }
     });
 }
