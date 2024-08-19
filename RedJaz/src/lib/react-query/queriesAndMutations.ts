e4a31d9d5aad1a2d70d886a3baa9c660c83ec4da
Jacobo <tadeo.jacobo6@gmail.com>
2024-08-19 15:13:16 -0400

    useSavePost mutation added

--- RedJaz/src/lib/react-query/queriesAndMutations.ts
+++ RedJaz/src/lib/react-query/queriesAndMutations.ts
@@ -1,5 +1,5 @@
 import {useQuery, useMutation, useQueryClient, useInfiniteQuery} from '@tanstack/react-query';
-import { createPost, createUserAccount, getRecentPosts, likePost, signInAccount, signOutAccount } from '../appwrite/api';
+import { createPost, createUserAccount, getRecentPosts, likePost, savePost, signInAccount, signOutAccount } from '../appwrite/api';
 import { INewPost, INewUser } from '@/types';
 import { QUERY_KEYS } from './queryKeys';
 
@@ -61,6 +61,22 @@ export const useLikePost = () => {
             })
         }
     })
-     
+}
 
+export const useSavePost = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: ({postId, userId}: {postId: string, userId: string}) => savePost(postId, userId),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_POSTS]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_CURRENT_USER]
+            })
+        }
+    })
 }
