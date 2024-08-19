b36a85b3a7ba8b76bc676d4fb732c0af706eb6d7
Jacobo <tadeo.jacobo6@gmail.com>
2024-08-19 15:06:52 -0400

    useLikePost mutation

--- RedJaz/src/lib/react-query/queriesAndMutations.ts
+++ RedJaz/src/lib/react-query/queriesAndMutations.ts
@@ -1,5 +1,5 @@
 import {useQuery, useMutation, useQueryClient, useInfiniteQuery} from '@tanstack/react-query';
-import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from '../appwrite/api';
+import { createPost, createUserAccount, getRecentPosts, likePost, signInAccount, signOutAccount } from '../appwrite/api';
 import { INewPost, INewUser } from '@/types';
 import { QUERY_KEYS } from './queryKeys';
 
@@ -40,4 +40,27 @@ export const useGetRecentPosts = () => {
         queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
         queryFn: () => getRecentPosts()
     });
+}
+
+export const useLikePost = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: ({postId, likesArray}: {postId: string, likesArray: string[]}) => likePost(postId, likesArray),
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_POSTS]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_CURRENT_USER]
+            })
+        }
+    })
+     
+
 }
