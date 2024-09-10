ca260526cf7241cba6c202bcec256f41a3c4f24f
Jacobo <tadeo.jacobo6@gmail.com>
2024-09-10 13:52:13 -0400

    Testing this version

--- RedJaz/src/_auth/forms/SigninForm.tsx
+++ RedJaz/src/_auth/forms/SigninForm.tsx
@@ -1,131 +1,120 @@
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { useToast } from "@/components/ui/use-toast"
-import { Input } from "@/components/ui/input"
-import { SigninValidation } from "@/lib/validation"
-import { Loader } from "lucide-react"
-import { Link, useNavigate } from "react-router-dom"
-import { useSignInAccount } from "@/lib/react-query/queriesAndMutations"
-import { useUserContext } from "@/context/AuthContext"
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Link, useNavigate } from "react-router-dom";
 
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Loader from "@/components/shared/Loader";
+import { useToast } from "@/components/ui/use-toast";
+
+import { SigninValidation } from "@/lib/validation";
+import { useSignInAccount } from "@/lib/react-query/queries";
+import { useUserContext } from "@/context/AuthContext";
 
 const SigninForm = () => {
-  const {toast} = useToast()
-  const {checkAuthUser, isLoading: isUserLoading} = useUserContext()
-  const navigate = useNavigate()
+  const { toast } = useToast();
+  const navigate = useNavigate();
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
 
+  // Query
+  const { mutateAsync: signInAccount, isLoading } = useSignInAccount();
 
-  const {mutateAsync: signInAccount} = useSignInAccount()
+  const form = useForm<z.infer<typeof SigninValidation>>({
+    resolver: zodResolver(SigninValidation),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof SigninValidation>>({
-      resolver: zodResolver(SigninValidation),
-      defaultValues: {
-        email: "",
-        password: "",
-      },
-    })
+  const handleSignin = async (user: z.infer<typeof SigninValidation>) => {
+    const session = await signInAccount(user);
 
-   
-    // 2. Define a submit handler.
-    async function onSubmit(values: z.infer<typeof SigninValidation>) {
-      console.log('we are here');
-  
-      try {
-        const session = await signInAccount({
-          email: values.email,
-          password: values.password,
-        });
-        console.log('Sign in result:', { session });
-  
-        if (!session) {
-          console.error('Sign in after signup failed');
-          return toast({
-            title: 'Sign in failed. Please try again',
-          });
-        }
-        console.log('Signed in successfully:', session);
-  
-        const isLoggedIn = await checkAuthUser();
-        console.log('Authentication check result:', { isLoggedIn });
-  
-        if (isLoggedIn) {
-          form.reset();
-          console.log('Navigating to home page');
-          navigate('/');
-        } else {
-          console.error('Authentication check failed');
-          return toast({
-            title: 'Authentication check failed. Please try again',
-          });
-        }
-      } catch (error) {
-        console.error('Error during sign in:', error);
-        toast({
-          title: 'An error occurred. Please try again',
-        });
-      }
+    if (!session) {
+      toast({ title: "Login failed. Please try again." });
+      
+      return;
     }
-  
+
+    const isLoggedIn = await checkAuthUser();
+
+    if (isLoggedIn) {
+      form.reset();
+
+      navigate("/");
+    } else {
+      toast({ title: "Login failed. Please try again.", });
+      
+      return;
+    }
+  };
+
   return (
     <Form {...form}>
-      <div className="sm:w-420 flex-center flex-col" >
+      <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/images/logo.svg" alt="logo" />
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12" >Log in to your account</h2>
-        <p className="text-light-3 small-medium md:base-regular" >It's nice to see you again. Please enter your details</p>
-     
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
-                <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="text" className="shad-input" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-                <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" className="shad-input" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className="shad-button_primary">
-          {isUserLoading ? (
-            <div className="flex-center gap-2" >
-              <Loader /> Loading...
-            </div>
-          ): "Sign in" }
+
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Log in to your account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          Welcome back! Please enter your details.
+        </p>
+        <form
+          onSubmit={form.handleSubmit(handleSignin)}
+          className="flex flex-col gap-5 w-full mt-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Email</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="shad-button_primary">
+            {isLoading || isUserLoading ? (
+              <div className="flex-center gap-2">
+                <Loader /> Loading...
+              </div>
+            ) : (
+              "Log in"
+            )}
           </Button>
-          <p className="text-small-regular text-light-2 text-center mt-2" >
-            Don't have an account?
-            <Link to="/sign-up" className="text-primary-500 text-small-semibold ml-1" >Sign up</Link>
+
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Don&apos;t have an account?
+            <Link
+              to="/sign-up"
+              className="text-primary-500 text-small-semibold ml-1">
+              Sign up
+            </Link>
           </p>
-      </form>
+        </form>
       </div>
     </Form>
-  )
-}
+  );
+};
 
-export default SigninForm
+export default SigninForm;
--- RedJaz/src/_auth/forms/SignupForm.tsx
+++ RedJaz/src/_auth/forms/SignupForm.tsx
@@ -1,172 +1,170 @@
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { useToast } from "@/components/ui/use-toast"
-import { Input } from "@/components/ui/input"
-import { SingupValidation } from "@/lib/validation"
-import { Loader } from "lucide-react"
-import { Link, useNavigate } from "react-router-dom"
-import { createUserAccount } from "@/lib/appwrite/api"
-import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations"
-import { useUserContext } from "@/context/AuthContext"
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Loader from "@/components/shared/Loader";
+import { useToast } from "@/components/ui/use-toast";
+
+import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queries";
+import { SignupValidation } from "@/lib/validation";
+import { useUserContext } from "@/context/AuthContext";
 
 const SignupForm = () => {
-  const {toast} = useToast()
-  const {checkAuthUser, isLoading: isUserLoading} = useUserContext()
-  const navigate = useNavigate()
+  const { toast } = useToast();
+  const navigate = useNavigate();
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
 
-  const {mutateAsync: createNewUserAccount, isPending: isCreatingAccount} = useCreateUserAccount()
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
-  const {mutateAsync: signInAccount, isPending: isSigningIn} = useSignInAccount()
+  // Queries
+  const { mutateAsync: createUserAccount, isLoading: isCreatingAccount } = useCreateUserAccount();
+  const { mutateAsync: signInAccount, isLoading: isSigningInUser } = useSignInAccount();
 
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof SingupValidation>>({
-      resolver: zodResolver(SingupValidation),
-      defaultValues: {
-        name: "",
-        username: "",
-        email: "",
-        password: "",
-      },
-    })
+  // Handler
+  const handleSignup = async (user: z.infer<typeof SignupValidation>) => {
+    try {
+      const newUser = await createUserAccount(user);
 
-   
-    // 2. Define a submit handler.
-    async function onSubmit(values: z.infer<typeof SingupValidation>) {
-      console.log('Submitting signup form with values:', values);
-    
-      const newUser = await createUserAccount(values);
       if (!newUser) {
-        console.error('Signup failed');
-        return toast({
-          title: 'Signup failed. Please try again',
-        });
+        toast({ title: "Sign up failed. Please try again.", });
+        
+        return;
       }
-      console.log('User created successfully:', newUser);
-    
+
       const session = await signInAccount({
-        email: values.email,
-        password: values.password,
+        email: user.email,
+        password: user.password,
       });
+
       if (!session) {
-        console.error('Sign in after signup failed');
-        return toast({
-          title: 'Sign in failed. Please try again',
-        });
+        toast({ title: "Something went wrong. Please login your new account", });
+        
+        navigate("/sign-in");
+        
+        return;
       }
-      console.log('Signed in successfully:', session);
-    
+
       const isLoggedIn = await checkAuthUser();
-      console.log('Authentication check result:', isLoggedIn);
-    
+
       if (isLoggedIn) {
         form.reset();
-        navigate('/');
+
+        navigate("/");
       } else {
-        console.error('Authentication check failed');
-        return toast({
-          title: 'Authentication check failed. Please try again',
-        });
+        toast({ title: "Login failed. Please try again.", });
+        
+        return;
       }
+    } catch (error) {
+      console.log({ error });
     }
-  
+  };
+
   return (
     <Form {...form}>
-      <div className="sm:w-420 flex-center flex-col" >
+      <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/images/logo.svg" alt="logo" />
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12" >Create a new account</h2>
-        <p className="text-light-3 small-medium md:base-regular" >To use RedJazz, please enter your account details</p>
-     
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input type="text" className="shad-input" {...field} />
-              </FormControl>
-              {/* <FormDescription>
-                This is your public display name.
-              </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-                <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input type="text" className="shad-input" {...field} />
-              </FormControl>
-              {/* <FormDescription>
-                This is your public display name.
-              </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-                <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="text" className="shad-input" {...field} />
-              </FormControl>
-              {/* <FormDescription>
-                This is your public display name.
-              </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-                <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" className="shad-input" {...field} />
-              </FormControl>
-              {/* <FormDescription>
-                This is your public display name.
-              </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className="shad-button_primary">
-          {isCreatingAccount ? (
-            <div className="flex-center gap-2" >
-              <Loader /> Loading...
-            </div>
-          ): "Sign up" }
+
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a new account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use snapgram, Please enter your details
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(handleSignup)}
+          className="flex flex-col gap-5 w-full mt-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Username</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Email</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="shad-button_primary">
+            {isCreatingAccount || isSigningInUser || isUserLoading ? (
+              <div className="flex-center gap-2">
+                <Loader /> Loading...
+              </div>
+            ) : (
+              "Sign Up"
+            )}
           </Button>
-          <p className="text-small-regular text-light-2 text-center mt-2" >
+
+          <p className="text-small-regular text-light-2 text-center mt-2">
             Already have an account?
-            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1" >Log in</Link>
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1">
+              Log in
+            </Link>
           </p>
-      </form>
+        </form>
       </div>
     </Form>
-  )
-}
+  );
+};
 
-export default SignupForm
+export default SignupForm;
